d313ec1fc75ed61833c2ee25abd0730f15e8275c
TheAlien707 <smg13@hampshire.edu>
2015-10-18 22:55:21 -0700

    Part1
    Make 16 White Cubes
    Make them turn red when clicked
    Make them turn back to white when another is clicked

---
+++ Assets/gameController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameController : MonoBehaviour {
+
+	public GameObject squareTile;
+    public GameObject[] allTiles;
+    public Material On;
+    public Material Off;
+    int numTiles = 16;
+
+    public void ProcessClickedTile(GameObject clickedTile)
+    {
+        foreach (GameObject oneTile in allTiles)
+        {
+            oneTile.GetComponent<Renderer>().material = Off;
+        }
+
+        clickedTile.GetComponent<Renderer>().material = On;
+        print("you clicked something");
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        allTiles = new GameObject[numTiles];
+        for (int i = 0; i < numTiles; i++)
+        {
+            allTiles[i] = (GameObject)Instantiate(squareTile, new Vector3(i * 2 - 14, 0, 10), Quaternion.identity);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/tileBehavior.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tileBehavior : MonoBehaviour {
+
+    public bool clickedOn = false;
+    public Material On;
+    public Material Off;
+    gameController aGameController;
+
+    void OnMouseDown()
+    {
+        aGameController.ProcessClickedTile(this.gameObject);
+    }
+
+    //Use this for initialization
+    void Start () {
+        aGameController = GameObject.Find("GameControllerObject").GetComponent<gameController>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    //original attempt w/o walkthrough; retain for contemplation later. 
+    //    void OnMouseDown ()
+    //    {
+    // when player clicks on tile, it remembers whether it's the first or second time it's been clicked
+    // causes multiple cubes to wind up red as each individual cube remembers its own bool
+    //        if (clickedOn == false)
+    //        {
+    //            clickedOn = true;
+    //            GetComponent<Renderer>().material = On;
+    //        }
+    //        else if (clickedOn == true)
+    //        {
+    //            clickedOn = false;
+    //            GetComponent<Renderer>().material = Off;
+    //        }
+    //    }
+}
